d05559eadc4268552deb07ab412a35333cc40ed5
Arun Krishanmoorthy <arunmba.rec@gmail.com>
2020-09-28 22:16:47 +0530

    Inital Commit

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyAiKSw9dUme20C9_c_PGb2I_Si1So7E__k",
+  authDomain: "productapp-9a837.firebaseapp.com",
+  databaseURL: "https://productapp-9a837.firebaseio.com",
+  projectId: "productapp-9a837",
+  storageBucket: "productapp-9a837.appspot.com",
+  messagingSenderId: "111987446269",
+  appId: "1:111987446269:web:1dbde5f7045a9054498994",
+  measurementId: "G-2Y3FNNTVTS",
 };
-
-
